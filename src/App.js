1150763b43a04eb5d6a250f80db165f1ff92941f
LEARNINGMATEKOL\arunit.mazumdar <arunit.mazumdar@learningmate.com>
2019-06-04 01:35:13 +0530

    updates pushed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+
 import AboutMe from './AboutMe/about-me.js'
+import ToDoList from './ToDoList/to-do-list.js'
 import Animation from './Animation/animation.js'
 import ParentChildComponent from './ParentChildCommunication/parent-child-communication.js'
 
@@ -9,15 +12,18 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+
         {<Animation />}
 
         {<AboutMe />}
 
-        {<ParentChildComponent/>}
+        {<ParentChildComponent />}
+
       </div>
     );
   }
---
+++ src/ToDoList/to-do-list.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class ToDoList extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div className='container parent-container'>
+                <h2>To Do List</h2>
+            </div>
+        )
+    }
+}
